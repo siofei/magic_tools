3068de2901ca4abfd63e8ddd3e86c93015b186d8
siofei <liu.xingfei@outlook.com>
2024-02-03 17:15:46 +0800

    1

---
+++ Scripts/12306.js
@@ -0,0 +1,17 @@
+let body = `
+{
+	"advertParam": {
+		"skipTime": 1
+	},
+	"materialsList": [
+		{
+			"creativeType": 1,
+			"billMaterialsId": "255",
+			"filePath": "h"
+		}
+	],
+	"message": "无广告返回",
+	"code": "00"
+}
+`
+$done({ body });
